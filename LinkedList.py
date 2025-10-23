674ac8e475f7ee17f8d0dea3a2fd7dfd1b376d69
AndresL230 <andreslopez.23061@gmail.com>
2025-10-22 23:37:47 -0400

    llist

--- LinkedList.py
+++ LinkedList.py
@@ -12,6 +12,35 @@ class LinkedList:
         self.head.next = self.tail
         self.tail.prev = self.head
 
+    def insBack(self, val):
+        node = Node(val)
 
-    def append(self, node):
+        node.next = self.tail
+        node.prev = self.tail.prev
+
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+    def insFront(self, val):
+        node = Node(val)
         
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
+
+    def removeFront(self):
+        self.head.next.next.prev = self.head
+        self.head.next = self.head.next.next
+        
+    def removeEnd(self):
+        self.tail.prev.prev.next = self.tail
+        self.tail.prev = self.tail.prev.prev
+
+    def print(self):
+        curr = self.head.next
+        while curr != self.tail:
+            print(curr.val, " -> ")
+            curr = curr.next
+        print()
